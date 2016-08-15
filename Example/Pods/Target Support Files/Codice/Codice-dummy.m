33697a555ee2318e9a2aaa0d128b347fe59f5de3
Fellipe Caetano <fellipe.caetano@movile.com>
2016-08-15 17:30:58 -0300

    Project creation

---
+++ Example/Pods/Target Support Files/Codice/Codice-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Codice : NSObject
+@end
+@implementation PodsDummy_Codice
+@end
---
+++ Example/Pods/Target Support Files/Pods-Codice_Tests/Pods-Codice_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Codice_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Codice_TestsVersionString[];
+
