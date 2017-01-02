9e070362cbf9beb28df7b1c8382b519c7fb5fc53
Fellipe Caetano <fellipe.caetano4@gmail.com>
2017-01-02 20:31:01 -0200

    Create a new library project

---
+++ Codice/Codice.h
@@ -0,0 +1,19 @@
+//
+//  Codice.h
+//  Codice
+//
+//  Created by Fellipe Caetano on 02/01/17.
+//  Copyright © 2017 Fellipe Caetano. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Codice.
+FOUNDATION_EXPORT double CodiceVersionNumber;
+
+//! Project version string for Codice.
+FOUNDATION_EXPORT const unsigned char CodiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Codice/PublicHeader.h>
+
+
